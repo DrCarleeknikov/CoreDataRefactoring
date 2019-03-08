0d8240e2aaba5c89b3655d5b7e82d875ca9b5db3
JoshB <bryson.joshua@gmail.com>
2019-03-08 11:14:29 -0700

    Initial Commit

---
+++ CoreDataRefactoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataRefactoring
+//
+//  Created by Joshua Bryson on 3/8/19.
+//  Copyright © 2019 Joshua Bryson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
